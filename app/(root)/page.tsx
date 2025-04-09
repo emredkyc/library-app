f94d5b489710b569bb7df53243fc07579e0cae97
emredkyc <emredkyc@hotmail.com>
2025-04-09 19:59:22 +0200

    create page root

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,30 @@
-import { Button } from "@/components/ui/button";
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { db } from "@/database/drizzle";
+import { books, users } from "@/database/schema";
+import { auth } from "@/auth";
+import { desc } from "drizzle-orm";
 
-const Home = () => (
+const Home = async () => {
+  const session = await auth();
+
+  const latestBooks = (await db
+    .select()
+    .from(books)
+    .limit(10)
+    .orderBy(desc(books.createdAt))) as Book[];
+
+  return (
     <>
-    <Button>Click me</Button>
+      <BookOverview {...latestBooks[0]} userId={session?.user?.id as string} />
+
+      <BookList
+        title="Latest Books"
+        books={latestBooks.slice(1)}
+        containerClassName="mt-28"
+      />
     </>
   );
+};
+
 export default Home;
